2d1d99264c0529800c08434c0d62d3a5088b19b8
gopinathbca35 <gopinathbca35@gmail.com>
2026-05-25 14:57:55 +0530

    change to '(backticks)

--- frontend/src/components/Record.jsx
+++ frontend/src/components/Record.jsx
@@ -53,7 +53,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("${API_URL}/record", {
+        response = await fetch(`${API_URL}/record`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
